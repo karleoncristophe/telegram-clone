79f9b562dbcd381640f8921d36ae9bdfae9c44fa
Karleon <karleoncristophe@hotmail.com>
2021-10-15 16:21:27 -0300

    removing user id

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,28 +1,27 @@
 const mongoose = require('../libs/Mongoose');
 const Schema = mongoose.Schema;
 const UserSchema = new mongoose.Schema(
-   {
-      _id: Schema.Types.ObjectId,
-      name: String,
-      username: String,
-      bio: String,
-      email: {
-         type: String,
-         lowercase: true,
-         unique: true,
-      },
+  {
+    name: String,
+    username: String,
+    bio: String,
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+    },
 
-      password: {
-         type: String,
-         select: false,
-      },
-   },
-   {
-      timestamps: {
-         createdAt: 'createdAt',
-         updatedAt: 'updatedAt',
-      },
-   }
+    password: {
+      type: String,
+      select: false,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+  }
 );
 
 const User = mongoose.model('User', UserSchema);
