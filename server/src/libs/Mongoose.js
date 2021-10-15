d9f76622b93aa59db31a62499b03eb89d716687f
Karleon <karleoncristophe@hotmail.com>
2021-10-15 15:06:03 -0300

    node_modules added

--- server/src/libs/Mongoose.js
+++ server/src/libs/Mongoose.js
@@ -3,16 +3,17 @@ const mongoose = require('mongoose');
 const DB = process.env.DB;
 
 const connect = async () => {
-    try {
-        const db = await mongoose.connect(DB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Mongodb is connected!');
-        return db;
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    const db = await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Mongodb is connected!');
+    return db;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 connect();
